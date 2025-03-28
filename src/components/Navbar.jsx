ea8ac4e83e745569473d4f3131be81c71dfba2cc
mohammaduzzal <aalp72142@gmail.com>
2025-03-28 11:07:54 +0600

    worked with navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,15 @@
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+  const links = <>
+  <li><NavLink to='/' className='hover:text-pink-500'>Home</NavLink></li>
+  <li><NavLink to='/portfolio' className='hover:text-pink-500'>Our Portfolio</NavLink></li>
+  <li><NavLink to='/team' className='hover:text-pink-500'>Our Team</NavLink></li>
+  <li><NavLink to='/contact' className='hover:text-pink-500'>Contact</NavLink></li>
+  </>
+
+
     return (
         <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
@@ -10,36 +20,24 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {
+          links
+        }
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <div>
+      <img className="w-[50px] h-[25px] md:w-[100px] md:h-[50px]" src={'/public/logo.png'} alt="" />
+    </div>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {
+        links
+      }
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <Link to='/auth/signin' className="btn">Login</Link>
   </div>
 </div>
     );
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../mainLayout/MainLayout";
 import Home from "../pages/Home";
+import Portfolio from "../pages/Portfolio";
+import Team from "../pages/Team";
+import Contact from "../pages/Contact";
+import AuthLayout from "../mainLayout/AuthLayout";
+import Signup from "../pages/Signup";
+import Signin from "../pages/Signin";
+import Error from "../components/Error";
 
 const router = createBrowserRouter([
     {
@@ -10,8 +17,38 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element: <Home></Home>
+            },
+            {
+                path:'/portfolio',
+                element: <Portfolio></Portfolio>
+            },
+            {
+                path:'/team',
+                element: <Team></Team>
+            },
+            {
+                path:'/contact',
+                element: <Contact></Contact>
+            },
+        ]
+    },
+    {
+        path:'auth',
+        element: <AuthLayout></AuthLayout>,
+        children:[
+            {
+                path:'/auth/signup',
+                element:<Signup></Signup>
+            },
+            {
+                path:'/auth/signin',
+                element:<Signin></Signin>
             }
         ]
+    },
+    {
+        path:'*',
+        element:<Error></Error>
     }
 ])
 export default router
